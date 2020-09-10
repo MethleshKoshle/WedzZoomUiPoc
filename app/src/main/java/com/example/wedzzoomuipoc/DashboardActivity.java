6a26a02467d21d16a134743a31756cd3c46c72ff
Methlesh Koshle <miitbh1@gmail.com>
2020-09-10 14:56:14 +0530

    2:55 PM

---
+++ app/src/main/java/com/example/wedzzoomuipoc/DashboardActivity.java
@@ -0,0 +1,46 @@
+package com.example.wedzzoomuipoc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import android.content.Intent;
+import android.os.Handler;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    public void onClickRoom1(View view) {
+        Intent intent = new Intent(this, ZoomMeetingActivity.class);
+        intent.putExtra("roomName", "Room-1"); //Put your id to your next Intent
+        startActivity(intent);
+    }
+
+    public void onClickRoom2(View view) {
+        Intent intent = new Intent(this, ZoomMeetingActivity.class);
+        intent.putExtra("roomName", "Room-2"); //Put your id to your next Intent
+        startActivity(intent);
+    }
+}
+
---
+++ app/src/main/java/com/example/wedzzoomuipoc/ZoomMeetingActivity.java
@@ -0,0 +1,64 @@
+package com.example.wedzzoomuipoc;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.content.Intent;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ZoomMeetingActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meeting);
+
+        String newString;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                newString= null;
+            } else {
+                newString= extras.getString("roomName");
+            }
+        } else {
+            newString= (String) savedInstanceState.getSerializable("roomName");
+        }
+        Toast toast = Toast.makeText(getApplicationContext(),
+                newString,
+                Toast.LENGTH_SHORT);
+        toast.show();
+
+        ImageView img = (ImageView) findViewById(R.id.imageView2);
+        TextView tv = (TextView)findViewById(R.id.textView);
+
+        assert newString != null;
+        if(newString.equals("Room-1")) {
+            img.setImageResource(R.drawable.wedz_2);
+            tv.setText("Room 1");
+        }
+        else {
+            img.setImageResource(R.drawable.wedz_3);
+            tv.setText("Room 2");
+        }
+    }
+}
